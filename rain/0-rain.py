44e52ccfe0c2e2baadb640d3de8f19481d601928
angeiradu <116736310+angeiradu@users.noreply.github.com>
2023-09-28 02:23:10 -0700

    0-rain.py

--- rain/0-rain.py
+++ rain/0-rain.py
@@ -1,37 +1,19 @@
 #!/usr/bin/python3
-# the amount of rainwater retained between the walls.
+"""the amount of rainwater retained between the walls"""
 
 def rain(walls):
-    water_temp = 0
+    """Calculate how much water will be retained after it rains."""
+    if not walls or len(walls) < 3:
+        return 0
+
     water = 0
-    size = len(walls)
-    prev_wall = 0
-
-    if size <= 0:
-        return water_temp
-
-    for i in range(size):
-
-        if walls[i] >= walls[prev_wall]:
-            prev_wall = i
-            water_temp = 0
-        else:
-            water += walls[prev_wall] - walls[i]
-            water_temp += walls[prev_wall] - walls[i]
-
-    if prev_wall < size - 1:
-
-        # Subtract last temp volume from total.
-        water -= water_temp
-        # Store last peak wall.
-        prev_pass_peak = prev_wall
-        prev_wall = size - 1
-
-        for i in range(size - 1, prev_pass_peak, -1):
-
-            if walls[i] >= walls[prev_wall]:
-                prev_wall = i
-            else:
-                water += walls[prev_wall] - walls[i]
+    for i in range(1, len(walls) - 1):
+        left = walls[i]
+        for j in range(i):
+            left = max(left, walls[j])
+        right = walls[i]
+        for j in range(i + 1, len(walls)):
+            right = max(right, walls[j])
+        water += min(left, right) - walls[i]
 
     return water
